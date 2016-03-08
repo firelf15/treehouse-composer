4809a25f6bd7799e5d880afaa41e0ef9b1d352f7
FirElf15 <github@firelf.com>
2016-03-07 21:29:26 -0800

    sanitize variables for contact

--- index.php
+++ index.php
@@ -24,5 +24,19 @@ $app->get('/contact', function() use($app) {
   $app->render('contact.twig'); // <-- SUCCESS
 })->name('contact');
 
+$app->post('/contact', function() use($app) {
+  $sender = $app->request->post('sender');
+  $email = $app->request->post('email');
+  $message = $app->request->post('message');
+  if(!empty('sender') && !empty('email') && !empty('message')) {
+    $cleanSender = filter_var($sender, FILTER_SANITIZE_STRING);
+    $cleanEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $cleanMessage = filter_var($message, FILTER_SANITIZE_STRING);
+  } else {
+    // feedback to user about an error
+    $app->redirect('/contact');
+  }
+})->name('contact');
+
 $app->run();
 
