dc345f50d8c9c6752765e06656d26132f711839d
FirElf15 <github@firelf.com>
2016-03-07 17:08:20 -0800

    some monolog stuff

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+date_default_timezone_set('America/Los_Angeles');
 require 'vendor/autoload.php';
 echo "hello";
-$log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
 $log->addWarning('Foo');
 ?>
