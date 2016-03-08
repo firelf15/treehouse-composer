ea73977fa4d27d8b4903b70556d1aa465d16acf4
FirElf15 <github@firelf.com>
2016-03-07 19:50:37 -0800

    add twig

--- index.php
+++ index.php
@@ -4,14 +4,13 @@ date_default_timezone_set('America/Los_Angeles');
 
 $app = new \Slim\Slim();
 
-$app->get('/', function () use ($app) {
+$app->get('/', function() use($app) {
   $app->render('index.html'); // <-- SUCCESS
 });
 
-$app->get('/contact', function () use ($app) {
+$app->get('/contact', function() use($app) {
   $app->render('contact.html'); // <-- SUCCESS
 });
 
 $app->run();
 
-?>
