e29df8732c53281dd2021ad25d0767fa3385e6c3
FirElf15 <github@firelf.com>
2016-03-07 20:46:58 -0800

    twig the templates for navigation and dry

--- index.php
+++ index.php
@@ -2,15 +2,27 @@
 require 'vendor/autoload.php';
 date_default_timezone_set('America/Los_Angeles');
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim( array (
+  'view' => new \Slim\Views\Twig()
+));
+
+$view = $app->view();
+$view->parserOptions = array(
+  'debug' => true
+);
+
+// https://github.com/slimphp/Slim-Views
+$view->parserExtensions = array(
+  new \Slim\Views\TwigExtension()
+);
 
 $app->get('/', function() use($app) {
-  $app->render('index.html'); // <-- SUCCESS
-});
+  $app->render('about.twig'); // <-- SUCCESS
+})->name('home');
 
 $app->get('/contact', function() use($app) {
-  $app->render('contact.html'); // <-- SUCCESS
-});
+  $app->render('contact.twig'); // <-- SUCCESS
+})->name('contact');
 
 $app->run();
 
