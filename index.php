f78ea264c2eafd908d5d8c78699f6af9cd73123c
FirElf15 <github@firelf.com>
2016-03-07 18:17:54 -0800

    more initial slim

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
 date_default_timezone_set('America/Los_Angeles');
 require 'vendor/autoload.php';
-echo "hello";
+// echo "hello";
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
-$log->addWarning('Foo');
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+// $log = new Logger('name');
+// $log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
+// $log->addWarning('Foo');
+
+$app = new \Slim\App;
+$app->get(
+  '/hello/{name}',
+  function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+  }
+);
+$app->run();
+
 ?>
